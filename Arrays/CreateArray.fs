a4306999957007b15d50603d7fe08139e5dbb7df
Michal <michalkoh@gmail.com>
2020-10-27 23:07:28 +0100

    Creation of arrays

--- Arrays/CreateArray.fs
+++ Arrays/CreateArray.fs
@@ -1,19 +1,22 @@
-module Arrays
+namespace FSharp.Playground
 
-let sample = [|10..20|]
+open System 
 
-let evenNumbers = [|for i in 10..100 do if i%2 = 0 then yield i|]
+module CreateArray =
 
-let files = 
-    System.IO.Directory.EnumerateFiles @"C:\Users\Michal Kohut\source\repos"
-    |> Array.ofSeq
+    let sample = 
+        [|10..20|]
 
-open System
+    let evenNumbers = [|for i in 10..100 do if i%2 = 0 then yield i|]
 
-let lastDays (year:int) =
-    Array.init 12 (fun i -> 
-        let month = i + 1
-        let firstDay = DateTime(year, month, 1)
-        let lastDay = firstDay.AddDays(float (DateTime.DaysInMonth(year, month) - 1))
-        lastDay
-    )
+    let files = 
+        System.IO.Directory.EnumerateFiles @"C:\Users\Michal Kohut\source\repos"
+        |> Array.ofSeq
+
+    let lastDays (year:int) =
+        Array.init 12 (fun i -> 
+            let month = i + 1
+            let firstDay = DateTime(year, month, 1)
+            let lastDay = firstDay.AddDays(float (DateTime.DaysInMonth(year, month) - 1))
+            lastDay
+        )
--- Program.fs
+++ Program.fs
@@ -1,6 +1,4 @@
-﻿open System
-
-[<EntryPoint>]
+﻿[<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
     0 // return an integer exit code

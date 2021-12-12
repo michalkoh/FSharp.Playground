0d4c2e5855f65eeb08e4807f48183c3d10b18f66
Michal <michalkoh@gmail.com>
2021-12-12 22:27:13 +0100

    Customer extended.

--- FSharp.Playground.Tests/Tests.fs
+++ FSharp.Playground.Tests/Tests.fs
@@ -5,28 +5,29 @@ module Tests =
     open Xunit
     open FSharp.Playground.Workshop.Types
     open FSharp.Playground.Workshop.Functions
+    open System
 
     [<Fact>]
     let ``Customer creation`` () =
-        let customer = { Id = 1; IsVip = false; Credit = 0M }
+        let customer = { Id = 1; IsVip = false; Credit = 0M<EUR>; PersonalDetails = { FirstName = "Michal"; Lastname = "Kohut"; DateOfBirth = DateTime(1984, 2, 24) }; Notifications = NoNotifications }
         Assert.Equal(1, customer.Id)
         Assert.Equal(false, customer.IsVip)
 
     [<Fact>]
     let ``getPurchases customer with even id`` () =
-        let customer = { Id = 1; IsVip = false; Credit = 0M }
+        let customer = { Id = 1; IsVip = false; Credit = 0M<EUR>; PersonalDetails = { FirstName = "Michal"; Lastname = "Kohut"; DateOfBirth = DateTime(1984, 2, 24) }; Notifications = NoNotifications }
         let (c, value) = getPurchases customer
-        Assert.Equal(80M, value)
+        Assert.Equal(80M<EUR>, value)
 
     [<Fact>]
     let ``getPurchases customer with odd id`` () =
-        let customer = { Id = 4; IsVip = false; Credit = 0M }
+        let customer = { Id = 4; IsVip = false; Credit = 0M<EUR>; PersonalDetails = { FirstName = "Michal"; Lastname = "Kohut"; DateOfBirth = DateTime(1984, 2, 24) }; Notifications = NoNotifications }
         let (c, value) = getPurchases customer
-        Assert.Equal(120M, value)
+        Assert.Equal(120M<EUR>, value)
 
     [<Fact>]
     let ``tryPromoteToVIP customer with purchase under 100 is not promoted`` () =
-        let customer = { Id = 1; IsVip = false; Credit = 10M }
+        let customer = { Id = 1; IsVip = false; Credit = 10M<EUR>; PersonalDetails = { FirstName = "Michal"; Lastname = "Kohut"; DateOfBirth = DateTime(1984, 2, 24) }; Notifications = NoNotifications }
         let functionChain = getPurchases >> tryPromoteToVip
         let result1 = functionChain customer
         Assert.False(result1.IsVip)
@@ -40,18 +41,18 @@ module Tests =
 
     [<Fact>]
     let ``tryPromoteToVIP customer with purchase above 100 is promoted`` () =
-        let customer = { Id = 2; IsVip = false; Credit = 10M }
+        let customer = { Id = 2; IsVip = false; Credit = 10M<EUR>; PersonalDetails = { FirstName = "Michal"; Lastname = "Kohut"; DateOfBirth = DateTime(1984, 2, 24) }; Notifications = NoNotifications }
         let result = customer |> getPurchases |> tryPromoteToVip
         Assert.True(result.IsVip)
 
     [<Fact>]
     let ``increase customer is vip`` () =
-        let customer = { Id = 1; IsVip = true; Credit = 100M }
+        let customer = { Id = 1; IsVip = true; Credit = 100M<EUR>; PersonalDetails = { FirstName = "Michal"; Lastname = "Kohut"; DateOfBirth = DateTime(1984, 2, 24) }; Notifications = NoNotifications }
         let c = increaseCreditUsingVip customer
-        Assert.Equal(200M, c.Credit)
+        Assert.Equal(200M<EUR>, c.Credit)
 
     [<Fact>]
     let ``increase customer is not vip`` () =
-        let customer = { Id = 1; IsVip = false; Credit = 100M }
+        let customer = { Id = 1; IsVip = false; Credit = 100M<EUR>; PersonalDetails = { FirstName = "Michal"; Lastname = "Kohut"; DateOfBirth = DateTime(1984, 2, 24) }; Notifications = NoNotifications }
         let c = increaseCreditUsingVip customer
-        Assert.Equal(150M, c.Credit)
+        Assert.Equal(150M<EUR>, c.Credit)
--- FSharp.Playground/Workshop/Functions.fs
+++ FSharp.Playground/Workshop/Functions.fs
@@ -6,18 +6,18 @@ module Functions =
 
     let getPurchases (customer : Customer) = 
         if customer.Id % 2 = 0 
-            then (customer, 120M)
-            else (customer, 80M)
+            then (customer, 120M<EUR>)
+            else (customer, 80M<EUR>)
 
-    let tryPromoteToVip (customerWithPurchases : Customer * decimal) =
+    let tryPromoteToVip (customerWithPurchases : Customer * decimal<EUR>) =
         let (customer, amount) = customerWithPurchases
-        if (amount > 100M)
+        if (amount > 100M<EUR>)
             then { customer with IsVip = true }
             else customer
 
     let increase condition customer = 
         if condition customer  
-            then { customer with Credit = customer.Credit + 100M }
-            else { customer with Credit = customer.Credit + 50M }
+            then { customer with Credit = customer.Credit + 100M<EUR> }
+            else { customer with Credit = customer.Credit + 50M<EUR> }
 
     let increaseCreditUsingVip = increase (fun c -> c.IsVip)
